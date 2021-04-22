9551959f430207703c1611e6999f859dfb6664ad
shubham <shubhamsinghshubham777@gmail.com>
2021-04-22 09:52:33 +0530

    Initial implementation of Responded page in ReceiverHomeActivity.kt

--- app/src/main/java/com/shubham/foodiedonor/views/ReceiverHomeActivity.kt
+++ app/src/main/java/com/shubham/foodiedonor/views/ReceiverHomeActivity.kt
@@ -1,19 +1,14 @@
 package com.shubham.foodiedonor.views
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.viewbinding.library.activity.viewBinding
 import androidx.viewpager.widget.ViewPager
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.shubham.foodiedonor.R
 import com.shubham.foodiedonor.databinding.ActivityReceiverHomeBinding
-import com.shubham.foodiedonor.views.fragments.donorHome.DonorDonationsFragment
-import com.shubham.foodiedonor.views.fragments.donorHome.DonorHomePageFragment
 import com.shubham.foodiedonor.views.fragments.donorHome.DonorViewPagerAdapter
 import com.shubham.foodiedonor.views.fragments.receiverHome.ReceiverDonationsFragment
-import com.shubham.foodiedonor.views.fragments.receiverHome.ReceiverProfileFragment
+import com.shubham.foodiedonor.views.fragments.receiverHome.ReceiverRespondedFragment
 import com.tuonbondol.keyboardutil.hideSoftKeyboard
 
 class ReceiverHomeActivity : AppCompatActivity() {
@@ -36,7 +31,7 @@ class ReceiverHomeActivity : AppCompatActivity() {
     private fun setupViewPager() {
         val adapter = DonorViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(ReceiverDonationsFragment(), "Donations")
-        adapter.addFragment(ReceiverProfileFragment(), "Profile")
+        adapter.addFragment(ReceiverRespondedFragment(), "Responded")
         binding.receiverHomeViewPager.apply {
             this.adapter = adapter
             offscreenPageLimit = 2
@@ -62,7 +57,7 @@ class ReceiverHomeActivity : AppCompatActivity() {
         binding.receiverHomePageSmartTabLayout.apply {
             setupWithViewPager(binding.receiverHomeViewPager)
             getTabAt(0)!!.setIcon(R.drawable.ic_heart_filled).text = "Donations"
-            getTabAt(1)!!.setIcon(R.drawable.ic_person).text = "Profile"
+            getTabAt(1)!!.setIcon(R.drawable.ic_history).text = "Responded"
         }
     }
 
---
+++ app/src/main/java/com/shubham/foodiedonor/views/fragments/receiverHome/ReceiverRespondedFragment.kt
@@ -0,0 +1,104 @@
+package com.shubham.foodiedonor.views.fragments.receiverHome
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.viewbinding.library.fragment.viewBinding
+import androidx.core.content.res.ResourcesCompat
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.shubham.foodiedonor.R
+import com.shubham.foodiedonor.databinding.FragmentReceiverDonationsBinding
+import com.shubham.foodiedonor.databinding.FragmentReceiverRespondedBinding
+import com.shubham.foodiedonor.models.DonorDonationModel
+import com.shubham.foodiedonor.models.ReceiverModel
+import com.shubham.foodiedonor.utils.Constants
+import com.shubham.foodiedonor.views.LoginActivity
+import com.shubham.foodiedonor.views.fragments.receiverHome.adapters.ReceiverDonationsPageListAdapter
+import dev.shreyaspatil.MaterialDialog.AbstractDialog
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
+import www.sanju.motiontoast.MotionToast
+
+class ReceiverRespondedFragment : Fragment(R.layout.fragment_receiver_responded) {
+
+    private val binding: FragmentReceiverRespondedBinding by viewBinding()
+    private lateinit var receiverDonationsPageListAdapter: ReceiverDonationsPageListAdapter
+    private val TAG = "ReceiverRespondedFragmentTAG"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        setupAllViews()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupAllViews() {
+        val currentReceiverMobile = requireActivity().getSharedPreferences(Constants.mySharedPrefName, Context.MODE_PRIVATE).getString("globalReceiverMobile", null)
+        Constants.globalReceiverCollectionRef.document(currentReceiverMobile!!).get()
+            .addOnSuccessListener {
+                val receiver = it.toObject<ReceiverModel>()
+                binding.apply {
+                    receiverRespondedPageName.text = "Welcome ${receiver?.name}"
+                    receiverRespondedPageSignoutBtn.setOnClickListener {
+
+                        val myDialog = MaterialDialog.Builder(requireActivity())
+                            .setAnimation(R.raw.logout)
+                            .setTitle("Confirm sign-out ${receiver?.name}?")
+                            .setMessage("Do you want to sign-out of this account?")
+                            .setPositiveButton("Yes", AbstractDialog.OnClickListener { dialogInterface, _ ->
+                                Firebase.auth.signOut()
+                                requireActivity().getSharedPreferences(Constants.mySharedPrefName, Context.MODE_PRIVATE).edit().clear().apply()
+                                val intent = Intent(requireActivity(), LoginActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                                startActivity(intent)
+                                dialogInterface.dismiss()
+                            })
+                            .setNegativeButton("No", AbstractDialog.OnClickListener { dialogInterface, _ ->
+                                dialogInterface.dismiss()
+                            }).build()
+
+                        myDialog.show()
+                    }
+                }
+            }
+            .addOnFailureListener {
+                MotionToast.createColorToast(requireActivity(), "Unexpected error occurred. Please try again later.", MotionToast.TOAST_ERROR, MotionToast.GRAVITY_BOTTOM, MotionToast.LONG_DURATION, ResourcesCompat.getFont(requireActivity(), R.font.alegreya_sans_sc_medium))
+                Log.d(TAG, "Error cause: ${it.localizedMessage}")
+            }
+    }
+
+    private fun setupRecyclerView() {
+
+        val sharedPreferences = requireActivity().getSharedPreferences(Constants.mySharedPrefName, Context.MODE_PRIVATE)
+        val currentReceiverMobileNumber = sharedPreferences.getString("globalReceiverMobile", null)
+        val query = Constants.globalReceiverCollectionRef.document(currentReceiverMobileNumber!!).collection("donationsReceived").orderBy("timestamp", Query.Direction.DESCENDING)
+        val options = FirestoreRecyclerOptions.Builder<DonorDonationModel>()
+            .setQuery(query, DonorDonationModel::class.java)
+            .build()
+        receiverDonationsPageListAdapter = ReceiverDonationsPageListAdapter(options)
+
+        binding.receiverRespondedRecyclerView.apply {
+            setHasFixedSize(true)
+            adapter = receiverDonationsPageListAdapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        receiverDonationsPageListAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        receiverDonationsPageListAdapter.stopListening()
+    }
+
+}
